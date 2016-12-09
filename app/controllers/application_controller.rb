ffe439c9c1fac29ad658b8df845ded506aacdfe6
Ignatius Reza <lyoneil.de.sire@gmail.com>
2016-12-09 19:34:35 +0900

    scope routes with locale which would be supplied through default_url_options
    test failed??

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
+
 end
